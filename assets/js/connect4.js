181744434815b7103cba19def00f2e7a97ad8cb6
Josiah <139285660+Jojobinx17@users.noreply.github.com>
2023-07-18 12:31:19 -0700

    start win algorithm testing

--- assets/js/connect4.js
+++ assets/js/connect4.js
@@ -152,3 +152,30 @@ function move(i = 42) {
 
 }
 
+function checkWin() {
+	
+	const wins = [  [0 ,1 ,2 ,3 ],  [1 ,2 ,3 ,4 ],  [2 ,3 ,4 ,5 ],  [3 ,4 ,5 ,6 ],
+					[7 ,8 ,9 ,10],  [8 ,9 ,10,11],  [9 ,10,11,12],  [10,11,12,13],
+					[14,15,16,17],  [15,16,17,18],  [16,17,18,19],  [17,18,19,20],
+					[21,22,23,24],  [22,23,24,25],  [23,24,25,26],  [24,25,26,27],
+					[28,29,30,31],  [29,30,31,32],  [30,31,32,33],  [31,32,33,34],
+					[35,36,37,38],  [36,37,38,39],  [37,38,39,40],  [38,39,40,41], ]
+	
+	for(var i = 0; i < wins.length; i++) {
+		
+		document.getElementById("s" + i[0]).innerHTML = "x";
+		document.getElementById("s" + i[1]).innerHTML = "x";
+		document.getElementById("s" + i[2]).innerHTML = "x";
+		document.getElementById("s" + i[3]).innerHTML = "x";
+								
+		alert(i);
+		
+		document.getElementById("s" + i[0]).innerHTML = "";
+		document.getElementById("s" + i[1]).innerHTML = "";
+		document.getElementById("s" + i[2]).innerHTML = "";
+		document.getElementById("s" + i[3]).innerHTML = "";
+	}
+}
+
+
+
