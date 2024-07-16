8fa74a2b62824f130dcc86b6c2e2deae562a70eb
Jojobinx17 <josiahbergen1@gmail.com>
2024-07-15 17:56:04 -0700

    this is why we write tests

--- assets/js/download.js
+++ assets/js/download.js
@@ -12,25 +12,21 @@ document.addEventListener('DOMContentLoaded', function() {
             fname = "duolingo-torture-main.zip";
             fpath = "files/duolingo-torture-main.zip";
             break;
-
-        default:
-            fname = "no valid file specified.";
-            break;
     }
 
     try {
         download(fpath, fname);
         document.getElementById("download-message").style.display = "block";
-    } finally {
         document.getElementById("file-name").innerHTML = 'downloading <strong>' + fname + '</strong>...'
+    }  catch {
+        document.getElementById("file-name").innerHTML = "<strong>no valid file specified!</strong>"
     }
-
 });
 
 function download(p, n) {
     const link = document.createElement("a");
     link.href = p;
     link.download = n;
-    link.click();
+    //link.click();
     document.getElementById("manual-link").href = p;
 }
